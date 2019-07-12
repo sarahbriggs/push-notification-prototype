73674e309e7879ec6d899ca2472f00f6ec4a7347
Sarah Briggs <sbriggs@20001941-MacBook-Pro.local>
2019-07-12 12:39:01 -0500

    subscribe button

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get 'subscription/create'
   root 'subscription#create'
 
+  get 'subscription/new'
+  root 'subscription#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
