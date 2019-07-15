a1c39b239620d015ab866a27161d6244b5f1d23f
Carolyn Blumberg <carolyn.blumberg@tastytrade.com>
2019-07-15 15:04:05 -0500

    added current_user helper method

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,6 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'  
   post '/login' => 'sessions#create'
-
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
