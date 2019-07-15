b58f11184d60b1f8016e070fa221fea29b9e9a0f
Carolyn Blumberg <carolyn.blumberg@tastytrade.com>
2019-07-15 10:22:53 -0500

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
 
   get 'subscription/new'
   root 'subscription#new'
+
+  get "users/index"
+  get "users/new"
+  get "users/create"
+  get "users/show"
+  resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
