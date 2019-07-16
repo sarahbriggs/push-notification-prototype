5b58e5e7a6bcdcd3a08eb031a238411abe064327
Sarah Briggs <sbriggs@20001941-MacBook-Pro.local>
2019-07-16 08:42:09 -0500

    get session token

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'  
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
