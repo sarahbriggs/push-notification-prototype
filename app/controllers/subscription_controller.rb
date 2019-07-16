2bb4f93b12c7cd346ddd76018231a5875371450d
Sarah Briggs <sbriggs@20001941-MacBook-Pro.local>
2019-07-16 10:16:26 -0500

    add subscription to db
    to do: subscribe those to topics

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -1,24 +1,22 @@
 class SubscriptionController < ApplicationController
 	def index
-		@traders = Trader.all
 		@subscriptions = Subscription.all
 	end
 
 	def new
 		@traders = Trader.all
-		@user = User.find(1)
-		@subscription = Subscription.new
 	end
 
 	def create
-		@user = params[:user]
+		@user = session[:user_id]
 		@trader = params[:trader]
 		@subscription = User.find(@user).subscriptions.create()
-		@subscription.save
-		# @subscription.belongs_to()
-		# @subscription.user_id = User.find(params[:user]).user_id
-		# @subscription.trader_id = Trader.find(params[:trader]).trader_id
-		# @subscription.save
+		@subscription.trader_id = @trader
+		if @subscription.save
+          redirect_to action: 'index', alert: "SUCCESS"
+      	else
+          redirect_to action: 'new', alert: "ERROR"
+      end
 	end 
 
 end
--- db/migrate/20190712152758_create_subscriptions.rb
+++ db/migrate/20190712152758_create_subscriptions.rb
@@ -3,8 +3,13 @@ class CreateSubscriptions < ActiveRecord::Migration[5.2]
     create_table :subscriptions do |t|
     	t.belongs_to :user, index: true 
     	t.belongs_to :trader, index: true 
-   
+
       t.timestamps
     end
+
+    add_index "subscriptions", [:user.user_id, :trader.trader_id], 
+    :name => "index_subscriptions_on_user_and_trader", :unique => true
+
+    validates_uniqueness_of :user.user_id, :trader.trader_id
   end
 end
