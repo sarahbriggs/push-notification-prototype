a1861291847cb516911a80dde4934b2e91b1cd64
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-24 14:00:18 -0500

    explicitly define routes for trader

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   resources :sessions
 
   get 'trader/index'
+  get 'trader/new' => 'trader#new'
+  post 'trader' => 'trader#create'
   resources :trader
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
