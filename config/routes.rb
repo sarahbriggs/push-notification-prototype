b601e74c69edbb4e4a603686efd038bd5e9db3c8
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-25 14:21:08 -0500

    test post req to platform_application/new

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   resources :trader
 
   post 'platform/new' => 'platform_application#new'
+  resources :platform_application
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
