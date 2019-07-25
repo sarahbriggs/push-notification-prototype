458cbe418f3e695c5c5e6a5d6c987a3632637ae6
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-25 14:33:48 -0500

    protect from forgery in platform app controller

--- app/controllers/platform_application_controller.rb
+++ app/controllers/platform_application_controller.rb
@@ -1,11 +1,13 @@
 class PlatformApplicationController < ApplicationController
+	protect_from_forgery :except => :create
+	
 	def new
         platform = params[:platform]
         platform_name = params[:platform_name]
-        
+
        	apns_private_key_path = params[:apns_private_key_path]
         apns_private_key_password = params[:apns_private_key_password]
-        
+
         Aws.config.update({
 			credentials: Aws::Credentials.new(ENV['AWSAccessKeyId'], ENV['AWSSecretKey']),
 			region: 'us-east-2'
@@ -16,7 +18,7 @@ class PlatformApplicationController < ApplicationController
 			file = File.read(apns_private_key_path)
           	p12 = OpenSSL::PKCS12.new(file, platform_apns_private_key_password)
           	cert = p12.certificate
-          	
+
           	attributes = {
             'PlatformCredential': p12.key.to_s,
             'PlatformPrincipal': cert.to_s 
