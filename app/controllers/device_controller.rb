d67cc1b139049ff1434463c8ae8a9e6938046c5d
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-26 16:11:47 -0500

    look up platform application arn

--- app/controllers/device_controller.rb
+++ app/controllers/device_controller.rb
@@ -1,4 +1,6 @@
 class DeviceController < ApplicationController
+	protect_from_forgery :except => :create
+
 	def create
 		@device = Device.new
 		@device.token = params[:token]
@@ -16,10 +18,14 @@ class DeviceController < ApplicationController
 		platform = params[:platform]
 		token = params[:token]
 
+		if (PlatformApplication.where(["platform_name = ?", platform)).exists?
+			@platform_application = PlatformApplication.where(["platform_name = ?", platform)
+		end 
+
 		if (Device.where(["device_token = ?", token])).exists?
 			@device = Device.where(["device_token = ?", token])
 		end 
-		
+
 		Aws.config.update({
 			credentials: Aws::Credentials.new(ENV['AWSAccessKeyId'], ENV['AWSSecretKey']),
 			region: ENV['AWSRegion']
@@ -27,8 +33,8 @@ class DeviceController < ApplicationController
 		sns_client ||= Aws::SNS::Client.new
 
 		resp = sns_client.create_platform_endpoint({
-		  platform_application_arn: "",
-		  token: ""
+		  platform_application_arn: @platform_application.platform_arn,
+		  token: @device.token
 		})
 
 		@device.endpoint_arn = resp.endpoint_arn
