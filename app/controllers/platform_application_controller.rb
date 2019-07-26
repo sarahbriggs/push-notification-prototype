b2aaf5bc87c885493bb448a1e0dc7d6ebe334fcb
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-26 09:09:02 -0500

    try getting attributes first

--- app/controllers/platform_application_controller.rb
+++ app/controllers/platform_application_controller.rb
@@ -2,47 +2,61 @@ class PlatformApplicationController < ApplicationController
 	protect_from_forgery :except => :create
 
 	def new
-        platform = params[:platform]
-        platform_name = params[:platform_name]
-
-        #apns_private_key_password = params[:apns_private_key_password]
-
-        Aws.config.update({
+		Aws.config.update({
 			credentials: Aws::Credentials.new(ENV['AWSAccessKeyId'], ENV['AWSSecretKey']),
 			region: 'us-east-1'
 		})
 		sns_client ||= Aws::SNS::Client.new
 
-		if ['APNS', 'APNS_SANDBOX'].include?(platform)
-			# file = File.read(apns_private_key_path)
-   #        	p12 = OpenSSL::PKCS12.new(file, apns_private_key_password)
-   #        	cert = p12.certificate
+		resp = sns_client.get_platform_application_attributes({
+			platform_application_arn: 'arn:aws:sns:us-east-1:877941893971:app/APNS_SANDBOX/testPlatformApplication'
+		})
 
-          	attributes = {
-            'PlatformCredential': ENV['APNS_PRIVATE_KEY'],
-            'PlatformPrincipal': ENV['APNS_CERT']
-        	}
-        end
+		render :json => {
+			:attributes => 
+			resp.attributes['arn:aws:sns:us-east-1:877941893971:app/APNS_SANDBOX/testPlatformApplication']
+		}
+  #       platform = params[:platform]
+  #       platform_name = params[:platform_name]
 
-        resp = sns_client.create_platform_application({
-            name: platform_name,
-            platform: platform,
-            attributes: attributes,
-        })
-        platform_arn = resp.platform_application_arn
+  #       #apns_private_key_password = params[:apns_private_key_password]
 
-        @platform_application = PlatformApplication.new 
-        @platform_application.platform_name = platform_name
-        @platform_application.platform_arn = platform_arn
+  #       Aws.config.update({
+		# 	credentials: Aws::Credentials.new(ENV['AWSAccessKeyId'], ENV['AWSSecretKey']),
+		# 	region: 'us-east-1'
+		# })
+		# sns_client ||= Aws::SNS::Client.new
 
-        if @platform_application.save
-			render :json => {
-				:platform_name => @platform_application.platform_name,
-				:platform_arn => @platform_application.platform_arn
-			}
-		else
-			render :json => {}
-		end
+		# if ['APNS', 'APNS_SANDBOX'].include?(platform)
+		# 	# file = File.read(apns_private_key_path)
+  #  #        	p12 = OpenSSL::PKCS12.new(file, apns_private_key_password)
+  #  #        	cert = p12.certificate
+
+  #         	attributes = {
+  #           'PlatformCredential': ENV['APNS_PRIVATE_KEY'],
+  #           'PlatformPrincipal': ENV['APNS_CERT']
+  #       	}
+  #       end
+
+  #       resp = sns_client.create_platform_application({
+  #           name: platform_name,
+  #           platform: platform,
+  #           attributes: attributes,
+  #       })
+  #       platform_arn = resp.platform_application_arn
+
+  #       @platform_application = PlatformApplication.new 
+  #       @platform_application.platform_name = platform_name
+  #       @platform_application.platform_arn = platform_arn
+
+  #       if @platform_application.save
+		# 	render :json => {
+		# 		:platform_name => @platform_application.platform_name,
+		# 		:platform_arn => @platform_application.platform_arn
+		# 	}
+		# else
+		# 	render :json => {}
+		# end
 	end 
 end
 
