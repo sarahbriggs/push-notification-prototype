36bee2d049de0d0b767b73b5402ff0686478f9cc
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-26 14:52:09 -0500

    switch to user devices for m - m relationship

--- db/migrate/20190726152035_create_devices.rb
+++ db/migrate/20190726152035_create_devices.rb
@@ -1,6 +1,7 @@
 class CreateDevices < ActiveRecord::Migration[5.2]
   def change
     create_table :devices do |t|
+    	t.string :platform_endpoint
     	t.string :device_token 
       t.timestamps
     end
