7b53edc1cfb870e173cbbe447d81c502c99e2aba
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-26 10:07:03 -0500

    temporary fix

--- app/controllers/platform_application_controller.rb
+++ app/controllers/platform_application_controller.rb
@@ -19,29 +19,31 @@ class PlatformApplicationController < ApplicationController
 
         platform = params[:platform]
         platform_name = params[:platform_name]
-        
-		if ['APNS', 'APNS_SANDBOX'].include?(platform)
-			# file = File.read(apns_private_key_path)
-   #        	p12 = OpenSSL::PKCS12.new(file, apns_private_key_password)
-   #        	cert = p12.certificate
 
-          	attributes = {
-            'PlatformCredential': ENV['APNS_PRIVATE_KEY'],
-            'PlatformPrincipal': ENV['APNS_CERT']
-        	}
-        end
+		# if ['APNS', 'APNS_SANDBOX'].include?(platform)
+		# 	# file = File.read(apns_private_key_path)
+  #  #        	p12 = OpenSSL::PKCS12.new(file, apns_private_key_password)
+  #  #        	cert = p12.certificate
 
-        resp = sns_client.create_platform_application({
-            name: platform_name,
-            platform: platform,
-            attributes: attributes,
-        })
-        platform_arn = resp.platform_application_arn
-        
+  #         	attributes = {
+  #           'PlatformCredential': ENV['APNS_PRIVATE_KEY'],
+  #           'PlatformPrincipal': ENV['APNS_CERT']
+  #       	}
+  #       end
+
+  #       resp = sns_client.create_platform_application({
+  #           name: platform_name,
+  #           platform: platform,
+  #           attributes: attributes,
+  #       })
+  #      platform_arn = resp.platform_application_arn
+
+  		platform_arn = 'arn:aws:sns:us-east-1:877941893971:app/APNS_SANDBOX/testPlatformApplication'
+  		
         @platform_application = PlatformApplication.new 
         @platform_application.platform_name = platform_name
         @platform_application.platform_arn = platform_arn
-        
+
         if @platform_application.save
 			render :json => {
 				:platform_name => @platform_application.platform_name,
