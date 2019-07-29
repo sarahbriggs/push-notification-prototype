658bb18ff713a267a86333173b439ed63ef8a337
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-29 15:17:27 -0500

    update routes and run migrations

--- app/controllers/user_device_controller.rb
+++ app/controllers/user_device_controller.rb
@@ -5,14 +5,14 @@ class UserDeviceController < ApplicationController
 		token = params[:token]
 		user_id = params[:user_id]
 		platform = params[:platform]
-		
+
 		@device = UserDevice.where("device_token = ? AND user_id = ?", token, user_id)
 		if !(@device.exists?)
-			@user = User.find(params[:user_id])
+			@user = User.find(user_id)
 			@device = @user.user_devices.create()
 			@device.device_token = token
 		end
-		
+
 		@platform_application = PlatformApplication.where("platform_name = ?", platform)
 
 		Aws.config.update({
@@ -20,7 +20,7 @@ class UserDeviceController < ApplicationController
 			region: ENV['AWSRegion']
 		})
 		sns_client ||= Aws::SNS::Client.new
-
+		
 		resp = sns_client.create_platform_endpoint({
 		  platform_application_arn: @platform_application.platform_arn,
 		  token: @device.device_token
