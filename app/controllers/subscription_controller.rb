d451286ecffb57c2e6ab2d8be67877ec3397a3d1
Carolyn Blumberg <carolyn.blumberg@tastytrade.com>
2019-08-06 15:15:20 -0500

    unsub

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -53,7 +53,7 @@ class SubscriptionController < ApplicationController
 
 		devices_list = @user.user_devices
 		for dev in devices_list.to_a do 
-			@subscription = Subscription.where(:trader_id => @trader.id, :user_device_id => dev.id).first
+			@sub = Subscription.where(:trader_id => @trader.id, :user_device_id => dev.id).first
 			@sub_arn = @sub.subscription_arn
 
 			if Subscription.destroy(@sub.id)
