63e380ac8162640fa9941aeabeca8171278190fd
Sarah Briggs <sarah.briggs@duke.edu>
2019-08-06 13:05:21 -0500

    remove platform_application

--- app/controllers/user_device_controller.rb
+++ app/controllers/user_device_controller.rb
@@ -16,10 +16,16 @@ class UserDeviceController < ApplicationController
 			@device = @dev.first 
 		end
 
-		@platform_application = PlatformApplication.where("platform_name = ?", platform)
+		if platform.eql? "APNS"
+			@platform_arn = ENV['APNS_ARN']
+		elsif platform.eql? "APNS_SANDBOX"
+			@platform_arn = ENV['APNS_SANDBOX_ARN']
+		elsif platform.eql? "GCM" 
+			@platform_arn = ENV['GCM_ARM']
+		end 
 
 		resp = @@sns_client.create_platform_endpoint({
-		  platform_application_arn: @platform_application.first.platform_arn,
+		  platform_application_arn: @platform_arn,
 		  token: @device.device_token
 		})
 		@device.device_endpoint = resp.endpoint_arn
@@ -35,7 +41,7 @@ class UserDeviceController < ApplicationController
 		# create new device-subscription for all user, trader pairs  
 		first_device = @user.user_devices.first 
 		subscription_list = first_device.subscriptions
-		
+
 		for subscription in subscription_list.to_a do
 			@trader = Trader.find(subscription.trader_id)
 			@device_subscription = @user.subscriptions.create()
--- db/migrate/20190725180958_create_platform_applications.rb
+++
@@ -1,9 +0,0 @@
-class CreatePlatformApplications < ActiveRecord::Migration[5.2]
-  def change
-    create_table :platform_applications do |t|
-    	t.string :platform_name 
-    	t.string :platform_arn
-      t.timestamps
-    end
-  end
-end
